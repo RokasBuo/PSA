4a7c36342f5a09046a84a09751f24604ca1c2577
Strazdonis <edvinasstrazdonis@gmail.com>
2022-03-09 15:14:46 +0200

    clean up index.js, load routes dynamically

--- Server/index.js
+++ Server/index.js
@@ -5,11 +5,8 @@ const express = require("express");
 const app = express();
 const db = require('./utils/db');
 const passport = require('passport');
-const { loadQuotes, getDailyQuote } = require('./utils/utils');
-const userQuotes = require('./models/user_quotes');
-// TODO: load routes dynamically
-const usersRouter = require('./routes/users');
-const quoteRouter = require('./routes/api/quotes/get-one');
+const { loadQuotes } = require('./utils/utils');
+const fs = require('fs');
 require('./utils/auth');
 app.use(express.json());
 
@@ -31,43 +28,22 @@ app.get("/", async (req, res) => {
     res.send("yep");
 });
 
-app.post("/quote", async (req, res) => {
-    if (!req.user) return res.status(401).send("unauthorized!");
-    const body = req.body;
-    console.log(body);
-    console.log(req.user);
-    let doc = await userQuotes.findOneAndUpdate({ user: req.user._id }, { quote: body.quote }, {
-        new: true, // return the inserted quote
-        upsert: true // Make this update into an upsert
-    });
-    console.log(doc);
-    return res.status(200).send("success");
-});
+processRoutePath(__dirname + "/routes");
 
-app.get("/quote", async (req, res) => {
-    if (!req.user) return res.status(401).send("unauthorized!");
-    const now = new Date();
-    now.setHours(0);
-    now.setMinutes(0);
-    now.setSeconds(0);
-    now.setMilliseconds(0);
-    // TODO: this would be prettier as a promise and async/await
-    userQuotes.findOne({
-        date: {
-            $gte: now
-        },
-        user: req.user._id
-    }, (err, doc) => {
-        if (err) throw err;
-        console.log(doc);
-        if (doc) { return res.json(doc); }
-        const quote = getDailyQuote(now, app.locals.quotes);
-        console.log(quote);
-        res.json(quote);
+function processRoutePath(route_path) {
+    fs.readdirSync(route_path).forEach(function(file) {
+        var filepath = route_path + '/' + file;
+        fs.stat(filepath, function(err,stat) {
+            if (stat.isDirectory()) {
+                processRoutePath(filepath);
+            } else {
+                console.info('Loading route: ' + filepath);
+                require(filepath)(app);
+            }
+        });
     });
-});
+}
 
-app.use('/users', usersRouter);
 
 app.use(express.static(path.join(__dirname, 'public')));
 

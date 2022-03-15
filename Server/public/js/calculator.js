0f83b865fa8d31f89a289dfb7aa287eca326eb15
Strazdonis <edvinasstrazdonis@gmail.com>
2022-03-15 14:56:50 +0200

    add calculation, localStorage functionality to calculator

--- Server/public/js/calculator.js
+++ Server/public/js/calculator.js
@@ -1,1 +1,29 @@
-console.log("hi");
+function getStorage() {
+    const json = window.localStorage.getItem("history");
+    const data = JSON.parse(json);
+    return data;
+}
+
+function saveStorage() {
+    const json = JSON.stringify(history);
+    window.localStorage.setItem("history", json);
+    return true;
+}
+
+let history = getStorage() || { inputs: [], results: [] };
+
+(async () => {
+    const form = document.querySelector("form");
+    form.addEventListener("submit", async e => {
+        e.preventDefault();
+        const formData = new FormData(form);
+        let input = formData.get("input");
+        const cleanInput = input.replaceAll("^", "**");
+        console.log(input, cleanInput);
+        const result = eval(cleanInput);
+        history.inputs.push(input);
+        history.results.push(result);
+        saveStorage();
+    });
+
+})();

3a4be13eaf8f48553ed58d02734091d43f19ccac
Strazdonis <edvinasstrazdonis@gmail.com>
2022-03-15 14:11:11 +0200

    only show dashboard if logged in

--- Server/routes/pages/dashboard.js
+++ Server/routes/pages/dashboard.js
@@ -1,7 +1,7 @@
 const path = require('path');
 module.exports = function (app) {
     app.get("/", async (req, res) => {
-       // if (!req.user) return res.redirect("/login");
+        if (!req.user) return res.redirect("/login");
         res.sendFile(path.join(appRoot + "/views/index.html"));
     });
 };

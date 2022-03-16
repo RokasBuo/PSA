8a4b1def2d0e5cd4a70d89ced5ebab46b081098a
Strazdonis <edvinasstrazdonis@gmail.com>
2022-03-16 23:29:20 +0200

    add pagination to calculator page - beautiful code, shit design

--- Server/public/js/calculator.js
+++ Server/public/js/calculator.js
@@ -1,3 +1,29 @@
+const tbody = document.querySelector("tbody");
+const form = document.querySelector("form");
+const inputEl = document.querySelector("input");
+const answerEl = document.getElementById("answer");
+const previousPage = document.getElementById("previousPage");
+const nextPage = document.getElementById("nextPage");
+const perPage = 10;
+let currentPage = 1;
+
+nextPage.addEventListener("click", () => {
+    if(currentPage+1 > Math.ceil(history.inputs.length / perPage)) {
+        return;
+    }
+    drawTable(++currentPage);
+    // show the current page somewhere
+});
+
+previousPage.addEventListener("click", () => {
+    if(currentPage-1 < 1) {
+        return;
+    }
+    drawTable(--currentPage);
+    // show the current page somwhere
+});
+
+
 function getStorage() {
     const json = window.localStorage.getItem("history");
     const data = JSON.parse(json);
@@ -12,18 +38,57 @@ function saveStorage() {
 
 let history = getStorage() || { inputs: [], results: [] };
 
-(async () => {
-    const form = document.querySelector("form");
-    form.addEventListener("submit", async e => {
-        e.preventDefault();
-        const formData = new FormData(form);
-        let input = formData.get("input");
-        const cleanInput = input.replaceAll("^", "**");
-        console.log(input, cleanInput);
-        const result = eval(cleanInput);
-        history.inputs.push(input);
-        history.results.push(result);
-        saveStorage();
-    });
+function generateCellHTML(input, result) {
+    return `<tr onclick="tdClickHandler('${input}', '${result}')">
+        <td data-label="Input: ">${input}</td>
+    </tr>`;
+}
 
-})();
+function generateTableHTML(page) {
+    console.log("called", page);
+    html = "";
+    const inputs = [...history.inputs].reverse();
+    const results = [...history.results].reverse();
+    console.log((page - 1) * perPage, page * perPage);
+    for (let i = (page - 1) * perPage; i < page * perPage; i++) {
+        console.log("loop :)");
+        const input = inputs[i];
+        const result = results[i];
+        if (!input) break;
+        html += generateCellHTML(input, result);
+    }
+    return html;
+}
+
+function tdClickHandler(input, result) {
+    console.log("CALLED");
+    inputEl.value = input;
+    answerEl.innerHTML = result;
+}
+
+function drawTable(page) {
+    console.log("page:", page);
+    tbody.innerHTML = generateTableHTML(page);
+}
+
+drawTable(currentPage);
+
+function resetStorage() {
+    history = { inputs: [], results: [] };
+    saveStorage();
+    drawTable(1);
+}
+
+form.addEventListener("submit", e => {
+    e.preventDefault();
+    const formData = new FormData(form);
+    let input = formData.get("input");
+    const cleanInput = input.replaceAll("^", "**");
+    console.log(input, cleanInput);
+    const result = eval(cleanInput);
+    history.inputs.push(input);
+    history.results.push(result);
+    answer.innerHTML = result;
+    drawTable(currentPage);
+    saveStorage();
+});

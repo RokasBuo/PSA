f024bdb660e54e3dfb4ce51217d197e24f06340b
Strazdonis <edvinasstrazdonis@gmail.com>
2022-03-16 21:36:26 +0200

    get /quote promisified the db operation, clean up code

--- Server/routes/api/quotes/get-one.js
+++ Server/routes/api/quotes/get-one.js
@@ -1,25 +1,37 @@
 const { getDailyQuote } = require('../../../utils/helpers');
 const userQuotes = require('../../../models/user_quotes');
+
+const findQuote = (now, uid) => {
+    return new Promise((resolve, reject) => {
+        userQuotes.findOne({
+            date: {
+                $gte: now
+            },
+            user: uid
+        }, (err, doc) => {
+            if (err) return reject(err);
+            return resolve(doc);
+        });
+    });
+};
+
+
 module.exports = function (app) {
     app.get("/quote", async (req, res) => {
-        if (!req.user) return res.status(401).json({ error: true, message: "You must be logged in"});
+        if (!req.user) return res.status(401).json({ error: true, message: "You must be logged in" });
         const now = new Date();
         now.setHours(0);
         now.setMinutes(0);
         now.setSeconds(0);
         now.setMilliseconds(0);
-        // TODO: this would be prettier as a promise and async/await
-        userQuotes.findOne({
-            date: {
-                $gte: now
-            },
-            user: req.user._id
-        }, (err, doc) => {
-            console.log(err, doc);
-            if (err) throw err;
-            if (doc) { return res.json({success: true, quote: doc.quote, author: req.user.username }); }
-            const quote = getDailyQuote(now, app.locals.quotes);
-            res.json({success: true, ...quote});
+        const doc = await findQuote(now, req.user._id).catch(err => {
+            console.error(err);
+            return res.status(500).json({ error: true, message: "Failed fetching a quote, error: " + err.message });
         });
+        if (doc) {
+            return res.json({ success: true, quote: doc.quote, author: req.user.username });
+        }
+        const quote = getDailyQuote(now, app.locals.quotes);
+        res.json({ success: true, ...quote });
     });
 };

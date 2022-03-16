c759f9b8b96742080a3ccd8de7417073a05a200c
Strazdonis <edvinasstrazdonis@gmail.com>
2022-03-16 21:22:18 +0200

    add some logging when starting the server

--- Server/index.js
+++ Server/index.js
@@ -42,8 +42,13 @@ function processRoutePath(route_path) {
 app.use(express.static(path.join(__dirname, 'public')));
 
 (async () => {
+    console.log("connecting to db...");
     const connection = await db.connect();
+    console.log("connected!");
+    console.log("loading quotes...");
     loadQuotes(app);
+    console.log("loaded!");
+    console.log("starting server...");
     await app.listen(config.port);
 
     console.log(`[${new Date().toISOString()}] web server active on port ${config.port}. http://localhost:${config.port}`);

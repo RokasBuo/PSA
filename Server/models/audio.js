da195ab1f85d60c63ddbcd3fb1f34baa030f318e
Strazdonis <edvinasstrazdonis@gmail.com>
2022-03-19 00:24:29 +0200

    add audio-memo model

---
+++ Server/models/audio.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+
+const schema = new Schema({
+    // foreign key relationship
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+    },
+    filename: {
+        type: String
+    }
+});
+
+module.exports = mongoose.model('audio_memos', schema);
